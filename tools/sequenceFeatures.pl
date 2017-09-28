6b7e71f39a2842f66dc2c426fbb50a35d601451c
tmcrys <32310922+tmcrys@users.noreply.github.com>
2017-09-28 18:24:45 +0200

    Update sequenceFeatures.pl

--- tools/sequenceFeatures.pl
+++ tools/sequenceFeatures.pl
@@ -2,31 +2,31 @@
 use strict;
 use warnings;
 
-####################
-#
-#    TMCrys version 0.1
-#    Copyright 2017 Julia Varga and Gábor E. Tusnády
-#
-#    If you use TMCrys, please cite: 
-#    Julia Varga and Gábor E. Tusnády TMCRys...
-#
-#
-#    This file is part of TMCrys.
-#
-#    TMCrys is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    TMCrys is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with TMCrys.  If not, see <http://www.gnu.org/licenses/>.
-#
-####################
+################################################################################
+#										#
+#    TMCrys version 0.1								#
+#    Copyright 2017 Julia Varga and Gábor E. Tusnády				#
+#										#
+#    If you use TMCrys, please cite: 						#
+#    Julia Varga and Gábor E. Tusnády TMCRys...					#
+#										#
+#										#
+#    This file is part of TMCrys.						#
+#										#
+#    TMCrys is free software: you can redistribute it and/or modify		#
+#    it under the terms of the GNU General Public License as published by	#
+#    the Free Software Foundation, either version 3 of the License, or		#
+#    (at your option) any later version.					#
+#										#
+#    TMCrys is distributed in the hope that it will be useful,			#
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of		#
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the		#
+#    GNU General Public License for more details.				#
+#										#
+#    You should have received a copy of the GNU General Public License		#
+#    along with TMCrys.  If not, see <http://www.gnu.org/licenses/>.		#
+#										#
+#################################################################################
 
 my $mydir = $ENV{'TMCRYS'};
 
